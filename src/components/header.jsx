6a0ddec0025de52e0f39251055be813974a3c483
GreenbullGit <green@DESKTOP-O91PG8I.localdomain>
2021-11-12 02:43:59 +0100

    v0.2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,8 @@
 export const Header = (props) => {
   return (
     <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
-                  {/* {props.data ? props.data.title : 'Loading'} */}
-                  <span></span>
-                </h1>
-                {/* <p>{props.data ? props.data.paragraph : 'Loading'}</p> */}
-                {/* <a
-                  href='#features'
-                  className='btn btn-custom btn-lg page-scroll'
-                >
-                  Learn More
-                </a>{' '} */}
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className='col-md-12 intro'>
+        <img src="img/intro-bg2.png" className="main-logo" alt="" />
       </div>
     </header>
   )

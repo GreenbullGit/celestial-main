af623858ce7056cd477a703c4a68592921e93a87
GreenbullGit <green@DESKTOP-O91PG8I.localdomain>
2021-11-16 18:03:59 +0100

    removed mobile hamburger

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,25 +3,14 @@ export const Navigation = (props) => {
     <nav id='menu' className='navbar navbar-default'>
       <div className='container'>
         <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
-          >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-          </button>
+          
           <a className='navbar-brand page-scroll' href='#page-top'>
             Celestial Body Shop
           </a>{' '}
         </div>
 
         <div
-          className='collapse navbar-collapse'
+          className='navbar-collapse'
           id='bs-example-navbar-collapse-1'
         >
           <ul className='nav navbar-nav navbar-right'>
